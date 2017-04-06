a8409a3d81df6a65f5d9cd011dabfdfce7ad6439
Conlan <conlanc@gmail.com>
2017-04-06 12:49:28 -0400

    Who knows if this will work, add build abilities to PHP script

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
-$filename = "sched.txt";
-$handle = fopen($filename, "r");
-$contents = fread($handle, filesize($filename));
-fclose($handle);
+$schedName = "sched.txt";
+$jarName = "ScheduleCheck-1.0-SNAPSHOT.jar";
+function main(){
+    global $schedName;
 
-$json = json_decode($contents);
-if (time() - $json->{'asOf'} > 120) {
-    exec("java -jar ScheduleCheck-1.0-SNAPSHOT.jar -q -j -f sched.txt");
+    if (getenv('HEROKU')) buildAndCopyJar();
+    if (!file_exists($schedName)) runAspenJar();
+
+    $handle = fopen($schedName, "r");
+    $contents = fread($handle, filesize($schedName));
+    fclose($handle);
+
+    $json = json_decode($contents);
+    if (time() - $json->{'asOf'} > 120) {
+        runAspenJar();
+    }
+
+    echo $contents;
 }
 
-echo $contents;
+function runAspenJar(){
+    global $jarName;
+
+    exec("java -jar $jarName -q -j -f sched.txt");
+}
+
+function buildAndCopyJar(){
+    global $jarName;
+
+    if (!file_exists("./$jarName") && file_exists("build.gradle")){
+        exec("./gradlew build");
+        if (copy("build/libs/$jarName", "./$jarName")){
+            error_log("Unable to copy and build jar!");
+        }
+    }
+}
