841bd21377bf898459f963c5f7edeead84cc6d40
HeroCC <herocc@herocc.com>
2017-08-12 13:23:24 -0400

    Don't crash when getting an invalid date

--- src/main/java/com/herocc/school/aspencheck/CSVParse.java
+++ src/main/java/com/herocc/school/aspencheck/CSVParse.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,10 +33,18 @@ public class CSVParse extends GenericEventGenerator {
         e.setTitle(record.get(1));
         e.setDescription(record.get(2));
         
-        LocalDateTime dtStart = LocalDate.parse(record.get(8), formatter).atStartOfDay();
-        e.setStartTime(dtStart);
+        LocalDateTime dtStart = LocalDateTime.MIN;
+        LocalDateTime dtEnd = LocalDateTime.MIN;
         
-        LocalDateTime dtEnd = LocalDate.parse(record.get(9), formatter).atTime(23, 59, 59);
+        try {
+          dtStart = LocalDate.parse(record.get(8), formatter).atStartOfDay();
+          dtEnd = LocalDate.parse(record.get(9), formatter).atTime(23, 59, 59);
+        } catch (DateTimeParseException dte) {
+          AspenCheck.log.warning("Event " + e.getTitle() + " has invalid date: " + dte.getParsedString());
+          dte.printStackTrace();
+        }
+  
+        e.setStartTime(dtStart);
         e.setEndTime(dtEnd);
         
         if (checkEventsOccurringNow) {
