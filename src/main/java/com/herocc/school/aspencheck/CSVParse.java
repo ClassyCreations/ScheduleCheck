cc9e13953e4abd9d7596d6c8293c4d3a0c9f7167
HeroCC <herocc@herocc.com>
2017-08-12 21:52:19 -0400

    Catch if the Spreadsheet isn't available

--- src/main/java/com/herocc/school/aspencheck/CSVParse.java
+++ src/main/java/com/herocc/school/aspencheck/CSVParse.java
@@ -27,38 +27,43 @@ public class CSVParse extends GenericEventGenerator {
       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
       ZoneId zoneId = ZoneId.systemDefault();
       
-      Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(new StringReader(csv));
-      for (CSVRecord record : records) {
-        Event e = new Event();
-        e.setTitle(record.get(1));
-        e.setDescription(record.get(2));
-        
-        LocalDateTime dtStart = LocalDateTime.MIN;
-        LocalDateTime dtEnd = LocalDateTime.MIN;
-        
-        try {
-          dtStart = LocalDate.parse(record.get(8), formatter).atStartOfDay();
-          dtEnd = LocalDate.parse(record.get(9), formatter).atTime(23, 59, 59);
-        } catch (DateTimeParseException dte) {
-          AspenCheck.log.warning("Event " + e.getTitle() + " has invalid date: " + dte.getParsedString());
-          dte.printStackTrace();
+      try {
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(new StringReader(csv));
+        for (CSVRecord record : records) {
+          Event e = new Event();
+          e.setTitle(record.get(1));
+          e.setDescription(record.get(2));
+    
+          LocalDateTime dtStart = LocalDateTime.MIN;
+          LocalDateTime dtEnd = LocalDateTime.MIN;
+    
+          try {
+            dtStart = LocalDate.parse(record.get(8), formatter).atStartOfDay();
+            dtEnd = LocalDate.parse(record.get(9), formatter).atTime(23, 59, 59);
+          } catch (DateTimeParseException dte) {
+            AspenCheck.log.warning("Event " + e.getTitle() + " has invalid date: " + dte.getParsedString());
+            dte.printStackTrace();
+          }
+    
+          e.setStartTime(dtStart);
+          e.setEndTime(dtEnd);
+    
+          if (checkEventsOccurringNow) {
+            long nowEpoch = LocalDateTime.now().atZone(zoneId).toEpochSecond();
+      
+            // If the start time has occurred (start - current epoch is negative)
+            // And the end time has not occurred (end - current is positive)
+            long diffStart = dtStart.atZone(zoneId).toEpochSecond() - nowEpoch;
+            long diffEnd = dtEnd.atZone(zoneId).toEpochSecond() - nowEpoch;
+      
+            if (diffStart < 0 && diffEnd > 0) events.add(e);
+          } else {
+            events.add(e);
+          }
         }
-  
-        e.setStartTime(dtStart);
-        e.setEndTime(dtEnd);
-        
-        if (checkEventsOccurringNow) {
-          long nowEpoch = LocalDateTime.now().atZone(zoneId).toEpochSecond();
-  
-          // If the start time has occurred (start - current epoch is negative)
-          // And the end time has not occurred (end - current is positive)
-          long diffStart = dtStart.atZone(zoneId).toEpochSecond() - nowEpoch;
-          long diffEnd = dtEnd.atZone(zoneId).toEpochSecond() - nowEpoch;
-          
-          if (diffStart < 0 && diffEnd > 0) events.add(e);
-        } else {
-          events.add(e);
-        }
+      } catch (NullPointerException npe) {
+        AspenCheck.log.warning("Error getting events from CSV, is it publicly readable and correctly formatted?");
+        npe.printStackTrace();
       }
     } catch (IOException e) {
       return events;
