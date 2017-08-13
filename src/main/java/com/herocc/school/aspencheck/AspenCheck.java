ce5d5e96047859c360f9a1bfb54aceb00020d94e
HeroCC <herocc@herocc.com>
2017-08-12 21:30:48 -0400

    Shuffle events list
    That way, eventually, each event gets a turn on the billboard when it refreshes

--- src/main/java/com/herocc/school/aspencheck/AspenCheck.java
+++ src/main/java/com/herocc/school/aspencheck/AspenCheck.java
@@ -18,6 +18,7 @@ import java.nio.file.Files;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.TimeZone;
@@ -102,6 +103,7 @@ public class AspenCheck {
     boolean halfDay = false;
     boolean noSchool = EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(LocalDate.now().getDayOfWeek());
     
+		Collections.shuffle(events);
     for (Event e : events) {
       if (e.getTitle().contains("Early Release")) halfDay = true;
       if (e.getTitle().contains("No School")) noSchool = true;
