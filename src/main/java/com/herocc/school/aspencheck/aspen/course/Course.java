57bb387e9f69890c406a0387b1ccd4cf9ae7951c
Conlan Cesar <conlanc@gmail.com>
2017-11-20 23:10:18 -0500

    Update Course to be serializable by Jackson

--- src/main/java/com/herocc/school/aspencheck/aspen/course/Course.java
+++ src/main/java/com/herocc/school/aspencheck/aspen/course/Course.java
@@ -4,13 +4,21 @@ import org.jsoup.nodes.Element;
 
 public class Course {
   
+  public String id;
+  public String name;
+  public String code;
+  public String term;
+  public String teacher;
+  public String room;
+  public String termGrade;
+  
   public Course(Element element) {
-    final String id = element.getElementsByTag("td").get(1).id().trim();
-    final String name = element.getElementsByTag("td").get(1).text().trim(); // Also possibly td[3]
-    final String code = element.getElementsByTag("td").get(2).text().trim();
-    final String term = element.getElementsByTag("td").get(4).text().trim();
-    final String teacher = element.getElementsByTag("td").get(5).text().trim();
-    final String room = element.getElementsByTag("td").get(6).text().trim();
-    final String termGrade = element.getElementsByTag("td").get(7).text().trim(); // TODO Map<> of all 4 quarter grades
+    id = element.getElementsByTag("td").get(1).id().trim();
+    name = element.getElementsByTag("td").get(1).text().trim(); // Also possibly td[3]
+    code = element.getElementsByTag("td").get(2).text().trim();
+    term = element.getElementsByTag("td").get(4).text().trim();
+    teacher = element.getElementsByTag("td").get(5).text().trim();
+    room = element.getElementsByTag("td").get(6).text().trim();
+    termGrade = element.getElementsByTag("td").get(7).text().trim(); // TODO Map<> of all 4 quarter grades
   }
 }
