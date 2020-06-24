23d251d193f39d523406000d10d25651d4bffb19
Conlan Cesar <conlanc@gmail.com>
2020-06-23 23:57:44 -0400

    Add license info to OpenAPI

--- src/main/java/com/herocc/school/aspencheck/config/OpenApiConfigurator.java
+++ src/main/java/com/herocc/school/aspencheck/config/OpenApiConfigurator.java
@@ -3,6 +3,7 @@ package com.herocc.school.aspencheck.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,8 @@ public class OpenApiConfigurator {
   @Bean
   public OpenAPI customOpenAPI() {
     return new OpenAPI().components(new Components())
-      .info(new Info().title("Aspen WebScrape REST API"));
+      .info(new Info()
+        .title("Aspen WebScrape REST API")
+        .license(new License().name("GPL-3.0").url("https://github.com/ClassyCreations/ScheduleCheck/blob/master/LICENSE")));
   }
 }
